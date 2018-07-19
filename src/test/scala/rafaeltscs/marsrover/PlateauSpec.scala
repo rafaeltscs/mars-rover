55f2af263dfc61e1f351d01a4d6a5ad1569343aa
Rafael Chiappetta <rafaeltscs@gmail.com>
2018-07-19 13:57:20 -0300

    fixes the position validation into the plateau for negative coordinates

--- src/test/scala/rafaeltscs/marsrover/PlateauSpec.scala
+++ src/test/scala/rafaeltscs/marsrover/PlateauSpec.scala
@@ -11,6 +11,10 @@ class PlateauSpec extends Specification {
     "Validating an invalid position" >> {
       val plateau: Plateau = Plateau(5,5)
       plateau.validatePosition(Position(6,6,Direction.NORTH)) must throwA[InvalidPositionException]
+      plateau.validatePosition(Position(-1,5,Direction.NORTH)) must throwA[InvalidPositionException]
+      plateau.validatePosition(Position(0,6,Direction.NORTH)) must throwA[InvalidPositionException]
+      plateau.validatePosition(Position(6,0,Direction.NORTH)) must throwA[InvalidPositionException]
+      plateau.validatePosition(Position(4,-1,Direction.NORTH)) must throwA[InvalidPositionException]
     }
   }
 
